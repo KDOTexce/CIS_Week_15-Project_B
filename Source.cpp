c140fdca148b296bf3c0f902916ca36499c7645e
KurtK <cooldude@yahoo.com>
2022-05-01 14:18:40 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+/*
+* Kurt Kangas
+* CIS 1202 R02
+* 05/01/2022
+* Programming Assignment #15b
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
