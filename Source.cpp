17bf1218113a1dc744cd85fed2db531ba859e4dd
KurtK <cooldude@yahoo.com>
2022-05-01 16:41:38 -0400

    Final Commit, Project done and working.

--- Source.cpp
+++ Source.cpp
@@ -10,14 +10,31 @@
 #include <algorithm>
 #include <iomanip>
 #include <vector>
-
+#include <type_traits>
 using namespace std;
+
+
+template <class T>
+T half(T number)
+{
+	string type = typeid(number).name();
+	if (type == "int")
+	{
+		return round(static_cast<float>((number / 2.0)));
+	}
+	return static_cast<float>(number / 2.0);
+}
 
 int main()
 {
-	cout << "\n";
 
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
 
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 
 
 
